0c103d05bab29ef8b8e5079164596cfab168d8ab
Chris <chris@steema.com>
2021-05-04 11:46:48 +0200

    Example of how to access the wrapped TChart instance through the Xaml wrapper.

--- TeeChart.Xaml.WPF Demo/Demos/BindingDataProperties.xaml.cs
+++ TeeChart.Xaml.WPF Demo/Demos/BindingDataProperties.xaml.cs
@@ -1,10 +1,20 @@
-﻿namespace XamlWPFDemo.Demos
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+
+namespace XamlWPFDemo.Demos
 {
-  public partial class BindingDataProperties
-  {
-    public BindingDataProperties()
+    public partial class BindingDataProperties
     {
-      InitializeComponent();
+        public BindingDataProperties()
+        {
+            InitializeComponent();
+
+            var internalChart = LogicalTreeHelper.GetChildren(tChart1).OfType<Steema.TeeChart.WPF.TChart>().First();
+
+            var internalColorGrid = (Steema.TeeChart.WPF.Styles.ColorGrid)internalChart[0];
+            internalColorGrid.Pen.Color = Colors.White;
+            internalColorGrid.Pen.Width = 2;
+        }
     }
-  }
 }
