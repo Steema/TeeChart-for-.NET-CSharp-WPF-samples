7ba1aba39fbd35baada0a2a8e24772b50e161c91
Chris Ireland <chris@steema.com>
2021-11-02 13:44:18 +0100

    Add ColorGrid sample

--- TeeChart.Xaml.WPF Demo/Demos/BindingDataProperties.xaml.cs
+++ TeeChart.Xaml.WPF Demo/Demos/BindingDataProperties.xaml.cs
@@ -5,6 +5,12 @@
     public BindingDataProperties()
     {
       InitializeComponent();
+
+      //var internalChart = LogicalTreeHelper.GetChildren(tChart1).OfType<Steema.TeeChart.WPF.TChart>().First();
+
+      //var internalColorGrid = (Steema.TeeChart.Styles.ColorGrid)internalChart[0];
+      //internalColorGrid.Pen.Color = System.Drawing.Color.White;
+      //internalColorGrid.Pen.Width = 2;
     }
   }
 }
--- TeeChart.Xaml.WPF Demo/Demos/BindingDataPropertiesViewModel.cs
+++ TeeChart.Xaml.WPF Demo/Demos/BindingDataPropertiesViewModel.cs
@@ -8,30 +8,36 @@ namespace XamlWPFDemo.Demos
   public class BindingDataPropertiesViewModel : ViewModelBase
   {
     private const int Height = 20;
-    private const int Amount = 20;
+    private const int Amount = 100;
 
     private double _offset;
 
     private readonly DispatcherTimer _timer;
 
-    public SeriesData[] Data { get; }
+    public Series3DData[] Data { get; }
 
     public BindingDataPropertiesViewModel()
     {
-      Data = new SeriesData[Amount];
-      for (var i = 0; i < Amount; i++)
+      Data = new Series3DData[Amount];
+
+      int i = 0;
+      for (int x = 0; x < 10; x++)
       {
-        Data[i] = new SeriesData {Y = Math.Sin(i / (double)Amount * Math.PI * 2) * Height};
+        for (int z = 0; z < 10; z++)
+        {
+          Data[i] = new Series3DData { X = x, Y = Math.Sin(i / (double)Amount * Math.PI * 2) * Height, Z = z };
+          i++;
+        }
       }
 
-      _timer = new DispatcherTimer(DispatcherPriority.Normal) {Interval = TimeSpan.FromSeconds(0.05)};
+      _timer = new DispatcherTimer(DispatcherPriority.Normal) { Interval = TimeSpan.FromSeconds(0.05) };
       _timer.Tick += OnTick;
       _timer.Start();
     }
 
     private void OnTick(object sender, EventArgs eventArgs)
     {
-      _offset += 0.5;
+      _offset += 2;
       for (var i = 0; i < Amount; i++)
       {
         Data[i].Y = Math.Sin((i + _offset) / Amount * Math.PI * 2) * Height;
